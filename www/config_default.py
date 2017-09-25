4d4a35f72b9439ffa9346c4f365718e7496512d1
JianBine <369932775@qq.com>
2017-09-25 23:37:33 +0800

    apis.py and config*.py and handler

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+'''
+Default configuration.
+'''
+
+__author__ = 'mibine'
+
+configs = {
+	'debug':True,
+	'db':{
+	    'host':'127.0.0.1',
+	    'port':3306,
+	    'user':'root',
+	    'password':'',
+	    'db':'awesome'
+	},
+	'session':{
+	    'secret':'Awesome'
+	}
+}
---
+++ www/config_override.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 
+'''
+Override configuration.
+'''
+__author__ = 'mibine'
+
+config = {
+	'db':{
+	    'host':'127.0.0.1'
+	}
+}
